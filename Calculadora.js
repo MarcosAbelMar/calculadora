4b4f7489cb582b458fefe2a3e0340fc12a5dacd8
Marcos Mar <marcosabel0810@gmail.com>
2023-01-20 00:56:08 -0600

    refactor: funciones flecha y nueva funcion

--- Calculadora.js
+++ Calculadora.js
@@ -1,56 +1,44 @@
-//Suma
-function myFunctionS(){  
-   var x,y,suma,resultado;  
-   x = document.getElementById("num1").value;  
-   y = document.getElementById("num2").value;  
+const funcionNumeros = () => {
+  x = document.getElementById("num1").value;  
+  y = document.getElementById("num2").value;  
+}
+const myFunctionSuma = () =>  { 
+  funcionNumeros();
    if (isNaN(x) || isNaN(y)) {  
      resultado = "Es necesarios introducir dos números válidos";  
    } else {    
      suma=parseFloat(x)+parseFloat(y);  
      resultado = suma;
    }  
-   document.getElementById("resultado").value=resultado;
-   document.getElementById("sumando").innerHTML = text;  
+   document.getElementById("resultado").value=resultado; 
 }
  //Resta
- function myFunctionR() {  
-    var x,y,resta,resultado;  
-    x = document.getElementById("num1").value;  
-    y = document.getElementById("num2").value;  
+const myFunctionResta = () => {     
     if (isNaN(x) || isNaN(y)) {  
       resultado = "Es necesarios introducir dos números válidos";  
     } else {    
       resta=parseFloat(x)-parseFloat(y);  
       resultado= resta;  
     }  
-    document.getElementById("resultado").value=resultado;
-    document.getElementById("restando").innerHTML = text;  
+    document.getElementById("resultado").value=resultado; 
   }  
   //Multiplicacion
-  function myFunctionM() {  
-    var x,y,multiplicacion,resultado;  
-    x = document.getElementById("num1").value;  
-    y = document.getElementById("num2").value;  
+  const myFunctionMultiplicacion = () => {   
     if (isNaN(x) || isNaN(y)) {  
       resultado = "Es necesarios introducir dos números válidos";  
     } else {    
       multiplicacion=parseFloat(x)*parseFloat(y);  
       resultado= multiplicacion;  
     }  
-    document.getElementById("resultado").value=resultado;
-    document.getElementById("multiplicando").innerHTML = text;  
+    document.getElementById("resultado").value=resultado; 
   }  
   //Division
-  function myFunctionD() {  
-    var x,y,division,resultado;  
-    x = document.getElementById("num1").value;  
-    y = document.getElementById("num2").value;  
+   const myFunctionDivision = () => {    
     if (isNaN(x) || isNaN(y)) {  
       resultado = "Es necesarios introducir dos números válidos";  
     } else {    
       division=parseFloat(x)/parseFloat(y);  
       resultado= division;  
     }  
-    document.getElementById("resultado").value=resultado;
-    document.getElementById("dividiendo").innerHTML = text;  
+    document.getElementById("resultado").value=resultado; 
   }  
